6970922b4207bfa69c8314f091e959f0c64ba879
unknown <dccabacungan@HED-DCCABACUNGA.fec.intra>
2015-01-06 09:28:43 +0800

    uploading files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Automated Evaluation System</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+
+</head>
+<body>
+
+<h1>Hi Master dacamon</h1>
+
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
